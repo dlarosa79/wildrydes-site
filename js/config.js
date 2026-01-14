7fbfd472509c7bb15b9702d6cd58da78288f74d1
dlarosa79 <david.larosa@gmail.com>
2026-01-14 19:34:25 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-southeast-1_v7YcabOKw', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7vh46ni4d5qjd1p4e046ah9fl0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '50429c76geqsahbtffqg1btemo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
